38b0536014c3b0305fb02551a980d40752695718
DanielGara <yo@danielgara.com>
2020-08-20 08:30:04 -0500

    Chapter5

---
+++ Chapter5/routes/todo-routes.ts
@@ -0,0 +1,46 @@
+import { Router } from "https://deno.land/x/oak/mod.ts";
+import { renderFileToString } from 'https://deno.land/x/dejs@0.8.0/mod.ts';
+
+const router = new Router();
+
+const todos: {id: String, name: String }[] = [
+    {id: "1",name: "Learn Deno"},
+    {id: "2",name: "Prepare lunch"},
+    {id: "3",name: "Read bible"}
+];
+
+router.get('/',async (ctx,next)=>{
+    const body = await renderFileToString(
+        Deno.cwd()+"/views/todo.ejs",
+        {
+            title: "My TODOs",
+            todos: todos,
+            error: null
+        }
+    );
+    ctx.response.body = body;
+});
+
+router.post('/add-todo',async (ctx,next)=>{
+    const newTodoTitle = (await ctx.request.body({type:"form"}).value).get("new-todo");
+    if(newTodoTitle && newTodoTitle.trim().length !== 0){
+        const newTodo = {
+            id : new Date().toISOString(),
+            name : newTodoTitle
+        };
+        todos.push(newTodo);
+        ctx.response.redirect("/");
+    }else{
+        const body = await renderFileToString(
+            Deno.cwd()+"/views/todo.ejs",
+            {
+                title: "My TODOs",
+                todos: todos,
+                error: "Field cannot be empty"
+            }
+        );
+        ctx.response.body = body;
+    }
+});
+
+export default router;
